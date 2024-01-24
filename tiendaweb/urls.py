a4b1efa7322abcbf4de3abb5cfea3094ecaab977
wilson2106-chacón <wilsonestebanchaconcontreras@gmail.com>
2024-01-24 15:04:18 -0500

    actualizacion rama wilson

--- tiendaweb/urls.py
+++ tiendaweb/urls.py
@@ -12,7 +12,7 @@ from tiendaweb.views import index
 urlpatterns = [
     path('', index, name='index'),
     path('login/',login_views, name='login'),
-     path('registro/',registro_views, name='registro'),
+    path('registro/',registro_views, name='registro'),
     path('admin/', admin.site.urls),
 
 ]
